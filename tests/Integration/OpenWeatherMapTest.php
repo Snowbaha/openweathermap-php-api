e8a6665c2af6a3240ab257ce6ff69a25ef0d06e8
André Pimpão <a.pimpao@programmator.dev>
2024-05-09 17:54:12 +0100

    tests: added air pollution resource tests

--- tests/Integration/OpenWeatherMapTest.php
+++ tests/Integration/OpenWeatherMapTest.php
@@ -2,6 +2,7 @@
 
 namespace ProgrammatorDev\OpenWeatherMap\Test\Integration;
 
+use ProgrammatorDev\OpenWeatherMap\Resource\AirPollutionResource;
 use ProgrammatorDev\OpenWeatherMap\Resource\GeocodingResource;
 use ProgrammatorDev\OpenWeatherMap\Resource\WeatherResource;
 use ProgrammatorDev\OpenWeatherMap\Test\AbstractTest;
@@ -10,7 +11,8 @@ class OpenWeatherMapTest extends AbstractTest
 {
     public function testMethods()
     {
-        $this->assertInstanceOf(GeocodingResource::class, $this->api->geocoding());
         $this->assertInstanceOf(WeatherResource::class, $this->api->weather());
+        $this->assertInstanceOf(AirPollutionResource::class, $this->api->airPollution());
+        $this->assertInstanceOf(GeocodingResource::class, $this->api->geocoding());
     }
 }
